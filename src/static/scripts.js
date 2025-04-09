a29d3ac0d216de201f1d2a511ddbf8636b11024d
alexk49 <alexcode9@mailbox.org>
2025-04-09 21:33:11 +0100

    updated form with tooltips and with better spacing. Added js to toggle file input vs text helpers.

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,7 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  /*
-  document.getElementById("back-arrow").addEventListener("click", () => {
-    history.back();
-  });
-  */
+function toggleDoiInputs() {
+  const fileRadio = document.getElementById('file_radio');
+  const textRadio = document.getElementById('text_radio');
+
+  const doisUpload = document.getElementById('dois_upload');
+  const doisText = document.getElementById('dois_text');
+  const helperContainer = document.getElementById('doi_upload_helper_container');
+
+  doisUpload.style.display = fileRadio.checked ? 'block' : 'none';
+  doisText.style.display = textRadio.checked ? 'block' : 'none';
+  helperContainer.style.display = fileRadio.checked ? 'block' : 'none';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fileRadio = document.getElementById('file_radio');
+  const textRadio = document.getElementById('text_radio');
+
+  fileRadio.addEventListener('click', toggleDoiInputs);
+  textRadio.addEventListener('click', toggleDoiInputs);
+
 });
