f1384f88cc117e32ab20d2317e352d071c469fb9
alexk49 <alexcode9@mailbox.org>
2025-04-10 10:07:17 +0100

    tidied layout and styles

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -2,16 +2,31 @@ function toggleDoiInputs() {
   const fileRadio = document.getElementById('file_radio');
   const textRadio = document.getElementById('text_radio');
 
-  const doisUpload = document.getElementById('dois_upload');
-  const doisText = document.getElementById('dois_text');
-  const helperContainer = document.getElementById('doi_upload_helper_container');
+  const doisUpload = document.getElementById('dois_upload_wrapper');
+  const doisText = document.getElementById('dois_text_wrapper');
 
   doisUpload.style.display = fileRadio.checked ? 'block' : 'none';
   doisText.style.display = textRadio.checked ? 'block' : 'none';
-  helperContainer.style.display = fileRadio.checked ? 'block' : 'none';
+}
+
+
+function countLines(event) {
+  const counterEl = document.getElementById('dois_text_counter');
+
+  const textarea = event.currentTarget;
+  const lines = textarea.value.split("\n");
+  const lineCount = lines.length;
+
+  if (lineCount !== 1) {
+    counterEl.textContent = `${lineCount} Dois added`;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const doisText = document.getElementById('dois_text');
+  doisText.addEventListener('input', countLines);
+
   const fileRadio = document.getElementById('file_radio');
   const textRadio = document.getElementById('text_radio');
 
