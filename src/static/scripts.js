2c76ddbf035b5f9769b159c0bba7f82177841ba7
alexk49 <alexcode9@mailbox.org>
2025-04-24 07:49:34 +0100

    Removed file-upload option to simplify form.

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,15 +1,3 @@
-function toggleDoiInputs() {
-  const fileRadio = document.getElementById('file_radio');
-  const textRadio = document.getElementById('text_radio');
-
-  const doisUpload = document.getElementById('dois_upload_wrapper');
-  const doisText = document.getElementById('dois_text_wrapper');
-
-  doisUpload.style.display = fileRadio.checked ? 'block' : 'none';
-  doisText.style.display = textRadio.checked ? 'block' : 'none';
-}
-
-
 function countLines(event) {
   const counterEl = document.getElementById('dois_text_counter');
 
@@ -63,15 +51,6 @@ async function handleFormSubmission(errorsDiv, messageDiv) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  const doisText = document.getElementById('dois_text');
-  doisText.addEventListener('input', countLines);
-
-  const fileRadio = document.getElementById('file_radio');
-  const textRadio = document.getElementById('text_radio');
-
-  fileRadio.addEventListener('click', toggleDoiInputs);
-  textRadio.addEventListener('click', toggleDoiInputs);
 
   const errorsDiv = document.getElementById('errors');
   const messageDiv = document.getElementById('message');
